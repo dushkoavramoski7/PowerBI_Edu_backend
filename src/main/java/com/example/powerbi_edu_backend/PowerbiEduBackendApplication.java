0ccddbfdf3b30f0975d41648240a6e80a107e14c
Dushko Avramoski <avramoski7@gmail.com>
2022-08-27 14:55:18 +0200

    Initial commit

---
+++ src/main/java/com/example/powerbi_edu_backend/PowerbiEduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerbi_edu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerbiEduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerbiEduBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/powerbi_edu_backend/PowerbiEduBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.powerbi_edu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerbiEduBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
