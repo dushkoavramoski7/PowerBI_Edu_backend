e00f882e5bb9e11ef648f184da5394743e7b83fb
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2022-09-08 15:39:29 +0200

    modified

--- src/main/java/com/example/powerbi_edu_backend/Model/Entities/Security/User.java
+++ src/main/java/com/example/powerbi_edu_backend/Model/Entities/Security/User.java
@@ -1,28 +1,27 @@
 package com.example.powerbi_edu_backend.Model.Entities.Security;
 
 
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long Id;
-//
-//    private String name;
-//
-//    private String username;
-//
-//    private String email;
-//
-//    private String password;
-//
-////    @ManyToOne(cascade = CascadeType.PERSIST)
-////    private Role role;
-//
-//}
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+
+    private String surname;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+}
--- src/main/java/com/example/powerbi_edu_backend/Repository/CommunityDocumentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.powerbi_edu_backend.Repository;
-
-import com.example.powerbi_edu_backend.Model.Entities.CommunityDocuments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommunityDocumentsRepository extends JpaRepository<CommunityDocuments, Long> {
-}
